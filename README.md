5a96d0938ee8499fb36f06d3697b7b4a7a4446a3
Nicholas Cortes <nicholascortes@Nicholass-MacBook-Air.local>
2022-10-15 19:54:15 -0700

    added more to questions and made a badfge generator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Read-Me-Generator
+
+<img width="1440" src="https://user-images.githubusercontent.com/110849715/191162801-4c49d6ef-3e31-4dc2-8ac2-0a17137140b1.png">
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
 const questions  = [
     {
       type: 'input',
       message: 'What is the title of your poject?',
       name: 'title',
+    },
+    {
+      //description question 1
+      type: 'input',
+      message: 'WWhat was your Github Username?',
+      name: 'gitUsername',
     },
     {
       //description question 1
@@ -33,10 +38,43 @@ const questions  = [
       name: 'descripQ4',
     },
     {
+      //Installation question
       type: 'input',
-      message: "If you used sources. List them seperated by commas",
+      message: "What are the steps necessary to install or acces your program",
+      name: 'Installation',  
+    },
+    {
+      //Usage question
+      type: 'input',
+      message: "Give instructions on how to operate your application and provide a screenshot of your application",
+      name: 'Usage',  
+    },
+    {
+      //third party Apis
+      type: 'input',
+      message: "If you used and thirs party sources list them seperated by commas",
       name: 'reference',  
-    }
+    },
+    {
+      //third party Apis
+      type: 'input',
+      message: "List your fellow collaborators followed by their Github profil link (Seperate them with commas after yo provide the link)",
+      name: 'collaborators',  
+    },
+    {
+      //third party Apis
+      type: 'input',
+      message: "Provide a link to a badge (optional)",
+      name: 'collaborators',  
+    },
+    {
+      type: 'list',
+      message: 'What license are you using?',
+      choices: [
+        "APM", "AUR license", "Bower" ,"Cocoapods" ,"Conda - License", "CPAN", "CRAN/METACRAN" , "Crates.io", "CTAN",  "DUB", "Eclipse Marketplace", "GitHub",  "GitLab"
+        ],
+      name: 'license',
+    },
   ]
   inquirer
   .prompt(questions)
@@ -44,15 +82,42 @@ const questions  = [
     console.log(response)
       var title = response.title;
       const re = /\s*(?:,|$)\s*/
+      const img = /\s*(?:https|$)\s*/
       //description tags
       var descriptionResponse = `${response.descripQ1},${response.descripQ2},${response.descripQ3},${response.descripQ4}`
       const descripARR = descriptionResponse.split(re)
-      console.log(descripARR)
+      //installation tags
+      var installation = response.installation
+      //usage tags
+      var usage = response.Usage
+      const usageARR = usage.split(img)
+      //collaborators
+      var collaborators = response.collaborators
+      const collaboratorsARR = collaborators.split(re)
+      //badges
+      var gitUsername = response.gitUsername
+      var license = response.license
+      var badgeURL = `https://img.shields.io/badge/License-${license}%202.0-blue.svg`
+      
+      //license
+      //references
       const reference = response.reference
+      const referenceARR = reference.split(re)
+      //must have https to finish link
+      fs.writeFile('Output.txt', badgeURL, (err) => {
       
-      console.log(reference)
-      
-     const nameList = reference.split(re)
-    
-      console.log(nameList)
+        // In case of a error throw err.
+        if (err) throw err;
+    })
   })
+
+  function writeFile(title, installation, usageARR, descripARR, referenceARR, collaboratorsARR) {
+    console.log(title)
+    console.log(installation)
+
+    console.log("https" + usageARR[1])
+    console.log(descripARR)
+    console.log(collaboratorsARR)
+        
+    console.log(referenceARR)
+  }
